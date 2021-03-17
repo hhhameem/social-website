470c789549f987e92af3958363c4d5cc0c30280c
hhhameem <hebronhamim@gmail.com>
2021-03-18 00:18:04 +0600

    add user registration and edit profile feature

--- socialsite/account/forms.py
+++ socialsite/account/forms.py
@@ -1,3 +1,4 @@
+from .models import Profile
 from django import forms
 from django.contrib.auth.models import User
 
@@ -21,3 +22,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Password doesn\'t match')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
